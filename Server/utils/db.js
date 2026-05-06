adc5cff053ebf0047444d05fcaa8ca50df2f864c
narakronkota <copter033x@gmail.com>
2026-05-06 21:52:41 +0700

    fix: updated db connection to cloud

--- Server/utils/db.js
+++ Server/utils/db.js
@@ -1,17 +1,21 @@
 import mysql from "mysql"
 
 const con = mysql.createConnection({
-  host: "localhost",
-  user: "root",
-  password: "",
-  database: "empoyee"
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+    port: process.env.DB_PORT,
+    // เข้ารหัสข้อมูลที่รับ-ส่งระหว่าง Render - avion 
+    ssl: {
+        rejectUnauthorized: false
+    }
 })
-
 con.connect((err) => {
   if(err){
     console.log("Database connection error")
   } else {
-    console.log("Connected to MySQL")
+    console.log("Connected to Aiven MySQL Cloud")
   }
 })
 
