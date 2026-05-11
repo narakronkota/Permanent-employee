8f4143b717923b791fb26cc2a35c446b80b7b11a
narakronkota <copter033x@gmail.com>
2026-05-11 17:06:09 +0700

    fix: add correct vercel url to cors

--- Server/index.js
+++ Server/index.js
@@ -12,7 +12,11 @@ import authroutes from './Routes/AuthRoute.js'
 const app = express()
 
 app.use(cors({
-  origin: ["http://localhost:5173", "https://emolyee-contact.vercel.app"],
+  origin: [
+        "http://localhost:5173", 
+        "https://emolyee-contact.vercel.app", 
+        "https://emolyee-contact-mrbl.vercel.app" 
+    ],
   methods: ["GET", "POST", "PUT", "DELETE"],
   credentials: true
 }));
