029aa37b6387c33b61eece581366d5ce13f85d99
narakronkota <copter033x@gmail.com>
2026-05-21 23:29:53 +0700

    cross edit

--- Server/index.js
+++ Server/index.js
@@ -11,7 +11,7 @@ const app = express()
 
 app.use(cors({
   origin: [
-    "https://emolyee-contact.vercel.app",
+    "https://staff-management-system-omega.vercel.app",
     "http://localhost:5173"
   ],
   methods: ["GET", "POST", "PUT", "DELETE"],
